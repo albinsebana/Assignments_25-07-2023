e9fe5076853d8fb67d44ce77c58c527bf1d082cd
Albin Sebana Kurian <100872558+albinsebana@users.noreply.github.com>
2023-08-03 17:23:43 +0530

    Boxing And Unboxing program

---
+++ BoxingUnboxingDemo/BoxingUnboxingDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxingUnboxingDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // assigned int value
+            // 23 to num
+            int num = 23;
+
+
+
+            // boxing
+           object boxedValue = num;
+
+            // unboxing
+            int i = (int)boxedValue;
+
+            // Display result
+            Console.WriteLine("Value of num (Orginal value) is : " + num);
+            Console.WriteLine("Value of boxedValue  is : " + boxedValue);
+            Console.WriteLine("Value of i(Unboxed value) is : " + i);
+        }
+    }
+}
